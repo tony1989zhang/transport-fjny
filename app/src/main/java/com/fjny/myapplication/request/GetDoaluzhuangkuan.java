f52584cab206d536949f3938c0883a3ca31e859f
1342434213 <1342434213@qq.com>
2019-12-18 11:23:03 +0800

    2019年12月18日 11:22:38 修改一出文字错误

---
+++ app/src/main/java/com/fjny/myapplication/request/GetDoaluzhuangkuan.java
@@ -0,0 +1,25 @@
+package com.fjny.myapplication.request;
+
+import android.content.Context;
+
+public class GetDoaluzhuangkuan extends BaseRequest {
+
+    public GetDoaluzhuangkuan(Context context) {
+        super(context);
+    }
+
+    @Override
+    String getUrl() {
+        return null;
+    }
+
+    @Override
+    String getParams() {
+        return null;
+    }
+
+    @Override
+    Object analyzeResponse(String responseString) {
+        return null;
+    }
+}
