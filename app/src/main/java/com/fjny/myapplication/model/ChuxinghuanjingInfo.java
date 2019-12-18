abc559cc11852c6c1e9b1db28b965526e97d07e5
1342434213 <1342434213@qq.com>
2019-12-18 10:26:51 +0800

    2019年12月18日 10:26:34 添加出行环境实体类

---
+++ app/src/main/java/com/fjny/myapplication/model/ChuxinghuanjingInfo.java
@@ -0,0 +1,49 @@
+package com.fjny.myapplication.model;
+
+public class ChuxinghuanjingInfo {
+    private int pm;
+    private int co2;
+    private int temp;
+    private int light;
+    private int humidity;
+
+    public int getPm() {
+        return pm;
+    }
+
+    public void setPm(int pm) {
+        this.pm = pm;
+    }
+
+    public int getCo2() {
+        return co2;
+    }
+
+    public void setCo2(int co2) {
+        this.co2 = co2;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public int getLight() {
+        return light;
+    }
+
+    public void setLight(int light) {
+        this.light = light;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+}
